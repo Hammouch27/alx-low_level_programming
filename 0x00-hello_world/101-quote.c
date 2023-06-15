8133b026dcbbbb8498163df623e1d980d5b19ac1
Hammouch27 <hmouche2001@gmail.com>
2023-06-15 12:21:54 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * Return: Allways 1.
+ */
+
+
+int main(void)
+
+{
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}			
+
+	
+
+
+
