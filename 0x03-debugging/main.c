23b35d1c947ddfd40a63bc049b47b18e1f876a5a
Hammouch27 <hmouche2001@gmail.com>
2023-06-21 11:11:15 -0700

    update

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
+ * main - Tests the positive_or_negative function
+ *
+ * This function is the entry point of the program. It tests the positive_or_negative function
+ * by calling it with the integer value of 98. The result of the function is printed to the console.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
         int i;
