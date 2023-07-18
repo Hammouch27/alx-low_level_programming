d17b53d2abc5789e360428eb43a7077bb6669681
Hammouch27 <hmouche2001@gmail.com>
2023-07-17 17:16:44 -0700

    0x0D preprocessor

--- 0x08-recursion/101-main.c
+++ 0x08-recursion/101-main.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     int r;
@@ -31,4 +36,3 @@ int main(void)
     printf("%d\n", r);
     return (0);
 }
-
